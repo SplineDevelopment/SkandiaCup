b415a3487b7b84a2b74ee64b0437468c3a4c8750
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-08-20 13:29:52 +0200

    Content from old repo

---
+++ Scandiacup/Scandiacup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scandiacup
+//
+//  Created by Jørgen Wilhelmsen on 17/08/15.
+//  Copyright (c) 2015 TeamTobb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
