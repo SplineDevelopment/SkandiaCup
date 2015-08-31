b6caf611fbe4bb7493d154c28cec03779803236c
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-08-31 20:12:25 +0200

    One more domain struct

---
+++ Skandiacup/Skandiacup/MatchTableRow.swift
@@ -0,0 +1,29 @@
+//
+//  MatchTableRow.swift
+//  Skandiacup
+//
+//  Created by Jørgen Wilhelmsen on 31/08/15.
+//  Copyright © 2015 Spline Development. All rights reserved.
+//
+
+import Foundation
+
+struct MatchTableRow{
+    var id: Int?
+    var matchClassId: String?
+    var matchGroupId: String?
+    var displayOrder: String?
+    var position: String?
+    var a: String?
+    var b: String?
+    var c: String?
+    var d: String?
+    var e: String?
+    var f: String?
+    var g: String?
+    var h: String?
+    var i: String?
+    var j: String?
+    var k: String?
+    var l: String?
+}
