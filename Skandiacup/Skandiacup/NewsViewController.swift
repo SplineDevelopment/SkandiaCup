4064dc33c467306bec72678383009d02780ffc3b
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-09-02 19:38:28 +0200

    SOAP + XML parser
    Added working code for posting SOAP-requests + parsing XML. Currently
    they are both located in NewsViewController.swift and is tested by
    pressing the button in that view. Messy - better structure later.

--- Skandiacup/Skandiacup/NewsViewController.swift
+++ Skandiacup/Skandiacup/NewsViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class NewsViewController: UIViewController {
+class NewsViewController: UIViewController, NSXMLParserDelegate {
+    @IBOutlet weak var textBox: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +22,56 @@ class NewsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func btnPressed(sender: AnyObject) {
+        let soapMessage = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:for='http://profixio.com/soap/tournament/ForTournamentExt.php'><soapenv:Header/><soapenv:Body><for:getTournamentMatchStatus><application_key>demo2015euro</application_key><tournamentID>11</tournamentID><since>2014-11-25 12:16:00</since></for:getTournamentMatchStatus></soapenv:Body></soapenv:Envelope>"
+        
+//        let soapMessage2 = "<?xml version='1.0' encoding='utf-8'?><soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'><soap:Body><CelsiusToFahrenheit xmlns='http://www.w3schools.com/webservices/'><Celsius>10</Celsius></CelsiusToFahrenheit></soap:Body></soap:Envelope>"
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://profixio.com/soap/tournament/ForTournamentExt.php")!)
+//        let request = NSMutableURLRequest(URL: NSURL(string: "http://www.w3schools.com/webservices/tempconvert.asmx")!)
+        request.HTTPMethod = "POST"
 
+        request.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.addValue(String(soapMessage.characters.count), forHTTPHeaderField: "Content-Length")
+
+        let postString = soapMessage
+        
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            print("response = \(response)")
+            
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+        
+      let xmlstring = "<hei v='asdasd'>a</hei>"
+
+        let data = (xmlstring as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+        let xmlparser = NSXMLParser(data: data!)
+        xmlparser.delegate = self
+        xmlparser.parse()
+    }
+
+    func parser(parser: NSXMLParser,didStartElement elementName: String, namespaceURI: String?,
+        qualifiedName qName: String?,attributes attributeDict: [String : String])
+    {
+        print("Element's name is \(elementName)")
+        print("Element's attributes are \(attributeDict)")
+        print("Element name is \(qName)")
+    }
+    
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        print("Elements value is \(string)")
+    }
+    
     /*
     // MARK: - Navigation
 
