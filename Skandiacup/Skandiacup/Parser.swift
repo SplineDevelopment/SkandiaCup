9ad08f29806493a1b3d09258769cf16021f82895
borgarlie <borgar.lie@gmail.com>
2015-09-03 15:12:48 +0200

    Removed generator lines from parser

--- Skandiacup/Skandiacup/Parser.swift
+++ Skandiacup/Skandiacup/Parser.swift
@@ -12,35 +12,9 @@ class Parser : NSObject, NSXMLParserDelegate {
     
     static let instance: Parser = Parser()
     
-    let soapMessage = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:for='http://profixio.com/soap/tournament/ForTournamentExt.php'><soapenv:Header/><soapenv:Body><for:getTournamentMatchStatus><application_key>demo2015uefa</application_key><tournamentID>11</tournamentID><since>2015-09-03 09:00:00</since></for:getTournamentMatchStatus></soapenv:Body></soapenv:Envelope>"
-    
     func parseTestArena() -> Arena? {
         let a = Arena()
         return a
-    }
-    
-    func test1() {
-        let request = NSMutableURLRequest(URL: NSURL(string: "http://profixio.com/soap/tournament/ForTournamentExt.php")!)
-        request.HTTPMethod = "POST"
-    
-        request.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        request.addValue(String(soapMessage.characters.count), forHTTPHeaderField: "Content-Length")
-    
-        let postString = soapMessage
-    
-        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
-            if error != nil {
-                print("error=\(error)")
-                return
-            }
-            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            let data = (responseString! ).dataUsingEncoding(NSUTF8StringEncoding)
-            let xmlparser = NSXMLParser(data: data!)
-            xmlparser.delegate = self
-            xmlparser.parse()
-        }
-        task.resume()
     }
     
     func parser(parser: NSXMLParser,didStartElement elementName: String, namespaceURI: String?,
