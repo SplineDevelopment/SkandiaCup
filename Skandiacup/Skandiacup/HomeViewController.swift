77d9629aed662db493746ff5af9ce25fc533f200
Bj√∏rn Hoxmark <bhoxmark@gmail.com>
2015-09-03 20:50:57 +0200

    Removed old "news" and added a new Home. Also wired up the containers in home to News, Info and sosial

--- Skandiacup/Skandiacup/HomeViewController.swift
+++ Skandiacup/Skandiacup/HomeViewController.swift
@@ -8,17 +8,63 @@
 
 import UIKit
 
-class HomeViewController: UIView {
+class HomeViewController: UIViewController {
 
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var infoView: UIView!
+    @IBOutlet weak var newsView: UIView!
+    @IBOutlet weak var sosialView: UIView!
+
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex{
+        case 0:
+            newsView.hidden = false
+            infoView.hidden = true
+            sosialView.hidden = true
+        case 1:
+            print("1")
+            newsView.hidden = true
+            infoView.hidden = false
+            sosialView.hidden = true
+
+        case 2:
+            newsView.hidden = true
+            infoView.hidden = true
+            sosialView.hidden = false
+
+        
+        default:
+            print("erroe")
+        }
+        
+        
+        
+    }
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        infoView.hidden = true
+        newsView.hidden = false
+        sosialView.hidden = true
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
+
     /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
     */
 
