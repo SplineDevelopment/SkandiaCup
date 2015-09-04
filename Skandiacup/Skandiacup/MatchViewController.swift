18883590e6770431de95caed53e11b30d9f5a5be
Bj√∏rn Hoxmark <bhoxmark@gmail.com>
2015-09-04 15:46:20 +0200

    UI til match med 2 constraints og endret navn på match til Tournament

--- Skandiacup/Skandiacup/MatchViewController.swift
+++ Skandiacup/Skandiacup/MatchViewController.swift
@@ -1,44 +1,61 @@
 //
-//  SecondViewController.swift
+//  MatchViewController.swift
 //  Skandiacup
 //
-//  Created by Jørgen Wilhelmsen on 31/08/15.
+//  Created by Bjørn Hoxmark on 04/09/15.
 //  Copyright © 2015 Spline Development. All rights reserved.
 //
 
 import UIKit
 
-class MatchViewController: UIViewController{
-    @IBOutlet weak var segmentControl: UISegmentedControl!
-    @IBOutlet weak var teamsView: UIView!
-    @IBOutlet weak var groupsView: UIView!
-
+class MatchViewController: UIViewController {
     
-    @IBAction func indexChanged(sender: AnyObject) {
-        switch segmentControl.selectedSegmentIndex {
-        case 0:
-            teamsView.hidden = false
-            groupsView.hidden = true
-        case 1:
-            teamsView.hidden = true
-            groupsView.hidden = false
-        default:
-            break;
-        }
+    @IBOutlet weak var flagOne: UILabel!
+    
+    @IBOutlet weak var scoreOne: UILabel!
+    
+    @IBOutlet weak var teamOne: UILabel!
+    
+    @IBOutlet weak var teamTwo: UILabel!
+    
+    @IBOutlet weak var scoreTwo: UILabel!
+    
+    @IBOutlet weak var flagTwo: UILabel!
+
+    @IBAction func btnPushed(sender: AnyObject) {
+        
+        //flagOne.text = "🇳🇴"
+        teamOne.text = "Rosenborg"
+        scoreOne.text = "3"
+        teamTwo.text = "Abugutha"
+        scoreTwo.text = "2"
+        //flagTwo.text = "🇰🇲"
+
+        
+        
         
     }
-    
-   override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        teamsView.hidden = false
-        groupsView.hidden = true
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
