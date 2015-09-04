b0d9da982aaa57cdbdd0d0290aa311772fa57f7a
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-09-04 11:48:00 +0200

    Added basic structure for MatchesMapper.

---
+++ Skandiacup/Skandiacup/MatchesMapper.swift
@@ -0,0 +1,23 @@
+//
+//  MatchesMapper.swift
+//  Skandiacup
+//
+//  Created by Jørgen Wilhelmsen on 04/09/15.
+//  Copyright © 2015 Spline Development. All rights reserved.
+//
+
+import Foundation
+
+class MatchesMapper: NSObject{
+    
+//    static func mapMatches(xml: XMLIndexer?) -> [TournamentMatch] {
+//        var arr: [TournamentMatch] = [TournamentMatch]()
+//        var lol = TournamentMatch()
+//        
+//        
+//        for elem in xml(["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:getMatchesResponse"]["getMatchesResult"]["item"]){
+//            arr.append(TournamentMatch(id: elem["id"].element?.text, matchTXID: <#T##Int?#>, matchno: <#T##String?#>, classId: <#T##Int?#>, homeTeamId: <#T##Int?#>, homeTeamName: <#T##String?#>, awayTeamId: <#T##String?#>, awayTeamName: <#T##String?#>, homeClubId: <#T##Int?#>, homeClubName: <#T##String?#>, awayClubId: <#T##Int?#>, awayClubName: <#T##String?#>, matchDate: <#T##String?#>, matchComment: <#T##String?#>, homeTeamText: <#T##String?#>, awayTeamText: <#T##String?#>, homegoal: <#T##String?#>, awaygoal: <#T##String?#>, fieldId: <#T##Int?#>, endGameLevel: <#T##Int?#>, sortOrder: <#T##String?#>, reason: <#T##String?#>, winner: <#T##String?#>, periodLengthInMinutes: <#T##String?#>, numberOfPeriodsInMatch: <#T##String?#>, UpdateTimeStamp: <#T##String?#>, matchGroupId: <#T##Int?#>))
+//        }
+//        xml["SOAP-ENV:Envelope"]["SOAP-ENV:Body"])
+//    }
+}
