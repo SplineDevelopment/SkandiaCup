a6ce262388a4d0cefc4e4741cd9b956ccaeb4db7
borgarlie <borgar.lie@gmail.com>
2015-09-04 16:02:22 +0200

    Made generator more general and added a club mapper and some more filters

--- Skandiacup/Skandiacup/Config.swift
+++ Skandiacup/Skandiacup/Config.swift
@@ -10,6 +10,5 @@ import Foundation
 
 class Config{
     static let soapMock = false //True = mock
-    
-    
+    static let appKeyTournamentID = "<application_key>demo2015uefa</application_key><tournamentID>11</tournamentID>"
 }
