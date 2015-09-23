c9e188730a39449448d57cc73d6a43a8feb1e64e
J√∏rgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-09-23 20:34:08 +0200

    Animation when pressing the search button. The search cell disappears when the button is pressed for hiding the cell - this is because cellForRowAtIndexPath doesnt return the search cell in these situations. Fix coming later

--- Skandiacup/Skandiacup/TeamsViewController.swift
+++ Skandiacup/Skandiacup/TeamsViewController.swift
@@ -4,13 +4,14 @@
 //
 //  Created by Jørgen Wilhelmsen on 03/09/15.
 //  Copyright © 2015 Spline Development. All rights reserved.
-//
 
 import UIKit
 
 class TeamsViewController: UIViewController , UITableViewDataSource, UITableViewDelegate{
     @IBOutlet weak var teamTableView: UITableView!
     @IBOutlet weak var segmentController: UISegmentedControl!
+    var searchPressed = false
+    var animated = false
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
@@ -21,14 +22,12 @@ class TeamsViewController: UIViewController , UITableViewDataSource, UITableView
                 self.teamTableView.reloadData()
                 self.activityIndicator.stopAnimating()
             })
-            
         }
     }
     
     @IBAction func indexChanged(sender: AnyObject) {
         (self.parentViewController?.parentViewController as! TournamentViewController).switchTable(segmentController.selectedSegmentIndex)
             viewDidLoad()
-        
     }
     
     override func viewDidLoad() {
@@ -39,7 +38,6 @@ class TeamsViewController: UIViewController , UITableViewDataSource, UITableView
         activityIndicator.hidden = false
         activityIndicator.startAnimating()
         segmentController.selectedSegmentIndex = 0
-        
         
         SharingManager.soap.getTeams(nil) { (teams) -> () in
             self.teams = teams
@@ -53,11 +51,28 @@ class TeamsViewController: UIViewController , UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("teamCell") as UITableViewCell!
-        cell.textLabel?.text = teams![indexPath.row].name
-        return cell
+//        print("ANIMATED: \(animated) SEARCHPRESSED: \(searchPressed) INDEXPATH ROW \(indexPath.row)")
+        if indexPath.row == 0 && searchPressed {
+            let cell = tableView.dequeueReusableCellWithIdentifier("searchCell") as UITableViewCell!
+            return cell
+        }
+        if searchPressed{
+            let cell = tableView.dequeueReusableCellWithIdentifier("teamCell") as UITableViewCell!
+            cell.textLabel?.text = teams![indexPath.row-1].name
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCellWithIdentifier("teamCell") as UITableViewCell!
+            cell.textLabel?.text = teams![indexPath.row].name
+            return cell
+        }
     }
     
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if(indexPath.row == 0 && searchPressed){
+            return 169
+        }
+        return 44
+    }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return teams != nil ? teams!.count : 0
@@ -65,6 +80,25 @@ class TeamsViewController: UIViewController , UITableViewDataSource, UITableView
     
     func changeSegment(){
         self.segmentController.selectedSegmentIndex = 0
+    }
+    
+    @IBAction func searchButtonPressed(sender: AnyObject) {
+        searchPressed = searchPressed ? false : true
+        if(!searchPressed){
+            let cell = self.teamTableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))
+            let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, 0, 200, 0)
+            let rotationTransform2 = CATransform3DTranslate(CATransform3DIdentity, 0, 169, 0)
+            cell!.layer.transform = rotationTransform
+            teamTableView.layer.transform = rotationTransform2
+            
+            UIView.animateWithDuration(1.0, animations: { () -> Void in
+                cell!.layer.transform = CATransform3DIdentity
+                self.teamTableView.layer.transform = CATransform3DIdentity
+                self.teamTableView.reloadData()
+                self.animated = false
+            })
+        }
+        self.teamTableView.reloadData()
     }
     
     // UITableViewDelegate Functions
@@ -86,13 +120,33 @@ class TeamsViewController: UIViewController , UITableViewDataSource, UITableView
         
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if (segue.identifier == "listToTeamView") {
-            if let indexPath = self.teamTableView.indexPathForSelectedRow{
-                let selectedTeam = teams![indexPath.row]
-                (segue.destinationViewController as! TeamViewController).currentTeam = selectedTeam
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if(indexPath.row == 0 && searchPressed){
+            if(!animated){
+                let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, 0, -200, 0)
+                let rotationTransform2 = CATransform3DTranslate(CATransform3DIdentity, 0, -169, 0)
+                cell.layer.transform = rotationTransform
+                teamTableView.layer.transform = rotationTransform2
+
+                UIView.animateWithDuration(1.0, animations: { () -> Void in
+                    cell.layer.transform = CATransform3DIdentity
+                    self.teamTableView.layer.transform = CATransform3DIdentity
+                    self.animated = true
+                })
             }
         }
     }
-
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "listToTeamView") {
+            if let indexPath = self.teamTableView.indexPathForSelectedRow{
+                if searchPressed{
+                    let selectedTeam = teams![indexPath.row-1]
+                    (segue.destinationViewController as! TeamViewController).currentTeam = selectedTeam
+                }else{
+                    let selectedTeam = teams![indexPath.row]
+                    (segue.destinationViewController as! TeamViewController).currentTeam = selectedTeam
+                }
+            }
+        }
+    }
 }
