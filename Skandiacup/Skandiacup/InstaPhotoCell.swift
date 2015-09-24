1a6492fb057807a50e0ae7240958656c70bee195
borgarlie <borgar.lie@gmail.com>
2015-09-24 12:29:37 +0200

    CollectionView fixed for instagram prototype

---
+++ Skandiacup/Skandiacup/InstaPhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  InstaPhotoCell.swift
+//  Skandiacup
+//
+//  Created by Borgar Lie on 24/09/15.
+//  Copyright © 2015 Spline Development. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InstaPhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    var instaPhotoObject : InstagramPhotoObject?
+}
--- Skandiacup/Skandiacup/InstagramRepoImpl.swift
+++ Skandiacup/Skandiacup/InstagramRepoImpl.swift
@@ -17,7 +17,21 @@ class InstagramRepoImpl : InstagramRepo {
     }
     
     func getAllPhotoObjects(completionHandler: (photoObjects: [InstagramPhotoObject]) -> ()) {
-        let arr = [InstagramPhotoObject]()
+        var arr = [InstagramPhotoObject]()
+        let url = NSURL(string: "http://images.freeimages.com/images/previews/23c/soccer-ball-and-grass-1550139.jpg")!
+        let urlSmall = NSURL(string: "http://images.freeimages.com/images/previews/23c/soccer-ball-and-grass-1550139.jpg")!
+        let published = Functions.getCurrentTimeInSeconds() - 1000
+        let user = "Test User"
+        let urlProfilePicture = NSURL(string: "http://images.freeimages.com/images/previews/a80/venetian-mask-1516474.jpg")!
+        let obj = InstagramPhotoObject(url: url, urlSmall: urlSmall, published: published, user: user, urlProfilePicture: urlProfilePicture)
+        let n = Int(arc4random_uniform(20))
+        print(n)
+        print("hei")
+        for i in 1...n {
+            arr.append(obj)
+        }
+        //arr.append(obj)
+        //arr.append(obj)
         completionHandler(photoObjects: arr)
     }
     
