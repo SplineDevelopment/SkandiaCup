d5dc8b85a804d21293ed7997d9ef90b670b3ccd1
Bj√∏rn Hoxmark <bhoxmark@gmail.com>
2015-09-25 15:06:21 +0200

    End play table added.

---
+++ Skandiacup/Skandiacup/EndPlayViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EndPlayViewController.swift
+//  Skandiacup
+//
+//  Created by Bjørn Hoxmark on 25/09/15.
+//  Copyright © 2015 Spline Development. All rights reserved.
+//
+
+import UIKit
+
+class EndplayViewController: UITableViewController{
+
+    
+    
+    
+}
+
