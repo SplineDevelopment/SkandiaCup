4ec14b3f388a9b8989d71ee2d08ad6b6e90302db
eiriksandberg <eirik.sandberg@live.no>
2015-09-26 16:15:25 +0200

    Added table to team view. Added headers to the view as well

---
+++ Skandiacup/Skandiacup/CustomHeaderCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomHeaderCell.swift
+//  Skandiacup
+//
+//  Created by Eirik Sandberg on 26.09.15.
+//  Copyright © 2015 Spline Development. All rights reserved.
+//
+
+import UIKit
+
+class CustomHeaderCell: UITableViewCell {
+    @IBOutlet var headerLabel: UILabel!
+}
---
+++ Skandiacup/Skandiacup/ResultTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ResultTableViewCell.swift
+//  Skandiacup
+//
+//  Created by Eirik Sandberg on 26.09.15.
+//  Copyright © 2015 Spline Development. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+    @IBOutlet weak var teamNameLabel: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var plusMinusLabel: UILabel!
+    @IBOutlet weak var lossesLabel: UILabel!
+    @IBOutlet weak var drawsLabel: UILabel!
+    @IBOutlet weak var winsLabel: UILabel!
+    @IBOutlet weak var gamesPlayedLabel: UILabel!
+}
