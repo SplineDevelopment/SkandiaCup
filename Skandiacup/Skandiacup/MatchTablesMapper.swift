b6e37bb6b6de8f1b964e6ac52d118c10e197a2df
J√∏rgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-10-08 18:21:00 +0200

    Changed Config to have appkey and tournamentid of the new tournament with more test data. Implemented (and tested) getting and mapping matchtable.

--- Skandiacup/Skandiacup/MatchTablesMapper.swift
+++ Skandiacup/Skandiacup/MatchTablesMapper.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class MatchTablesMapper: NSObject {
     static func mapMatchTables(xml: XMLIndexer) -> [MatchTable] {
-        let arr: [MatchTable] = [MatchTable]()
+        var arr: [MatchTable] = [MatchTable]()
 
         for elem in xml["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns1:getTableResponse"]["getTableResult"]["item"]{
             var matchtable: MatchTable = MatchTable()
@@ -20,65 +20,60 @@ class MatchTablesMapper: NSObject {
                 matchtable.header = mapMatchTableRow(elem["Header"])
             }
             
-            var matchtablerow: MatchTableRow
             if elem["Rows"].element != nil{
                 for row in elem["Rows"]["item"]{
-                    matchtablerow = mapMatchTableRow(row)
-                    matchtable.rows?.append(matchtablerow)
+                    matchtable.rows?.append(mapMatchTableRow(row))
                 }
             }
-            
+            arr.append(matchtable)
         }
         return arr
     }
     
-    private static func mapMatchTableRow(xml: XMLIndexer) -> MatchTableRow{
+    private static func mapMatchTableRow(elem: XMLIndexer) -> MatchTableRow{
         var row = MatchTableRow()
+        if elem["id"].element?.text != nil{
+            row.id = Int((elem["id"].element?.text )!)
+        }
         
-        for elem in xml.children{
-            if elem["id"].element?.text != nil{
-                row.id = Int((elem["id"].element?.text )!)
-            }
-            
-            if elem["MatchClassId"].element?.text != nil{
-                row.matchClassId = elem["MatchClassId"].element?.text
-            }
-            
-            if elem["MatchGroupId"].element?.text != nil{
-                row.matchGroupId = elem["MatchGroupId"].element?.text
-            }
-            
-            if elem["DisplayOrder"].element?.text != nil{
-                row.displayOrder = elem["DisplayOrder"].element?.text
-            }
-            
-            if elem["Position"].element?.text != nil{
-                row.position = elem["Position"].element?.text
-            }
-            
-            if elem["a"].element?.text != nil{
-                row.a = elem["a"].element?.text
-            }
-            
-            if elem["b"].element?.text != nil{
-                row.b = elem["b"].element?.text
-            }
-            
-            if elem["c"].element?.text != nil{
-                row.c = elem["c"].element?.text
-            }
-            
-            if elem["d"].element?.text != nil{
-                row.d = elem["d"].element?.text
-            }
-            
-            if elem["e"].element?.text != nil{
-                row.e = elem["e"].element?.text
-            }
-            
-            if elem["f"].element?.text != nil{
-                row.f = elem["f"].element?.text
-            }
+        if elem["MatchClassId"].element?.text != nil{
+            row.matchClassId = elem["MatchClassId"].element?.text
+        }
+        
+        if elem["MatchGroupId"].element?.text != nil{
+            row.matchGroupId = elem["MatchGroupId"].element?.text
+        }
+        
+        if elem["DisplayOrder"].element?.text != nil{
+            row.displayOrder = elem["DisplayOrder"].element?.text
+        }
+        
+        if elem["Position"].element?.text != nil{
+            row.position = elem["Position"].element?.text
+        }
+        
+        if elem["a"].element?.text != nil{
+            row.a = elem["a"].element?.text
+        }
+        
+        if elem["b"].element?.text != nil{
+            row.b = elem["b"].element?.text
+        }
+        
+        if elem["c"].element?.text != nil{
+            row.c = elem["c"].element?.text
+        }
+        
+        if elem["d"].element?.text != nil{
+            row.d = elem["d"].element?.text
+        }
+        
+        if elem["e"].element?.text != nil{
+            row.e = elem["e"].element?.text
+        }
+        
+        if elem["f"].element?.text != nil{
+            row.f = elem["f"].element?.text
         }
         return row
     }
