63b9525f2e58e65a3e7bb035adf9780a6f9a2225
Bj√∏rn Hoxmark <bhoxmark@gmail.com>
2015-10-14 18:41:25 +0200

    Code cleanup.

--- Skandiacup/Skandiacup/HomeViewController.swift
+++ Skandiacup/Skandiacup/HomeViewController.swift
@@ -31,7 +31,6 @@ class HomeViewController: UIViewController {
         infoView.hidden = true
         sosialView.hidden = true
         headerLabel.hidden = true
-        //segmentedControl.selectedSegmentIndex = 1
     }
     
     func showInfoTab(){
@@ -40,8 +39,6 @@ class HomeViewController: UIViewController {
         infoView.hidden = false
         sosialView.hidden = true
         headerLabel.hidden = true
-        //segmentedControl.selectedSegmentIndex = 2
-       
         self.callViewChangedToChildController(InfoViewController)
     }
     
@@ -51,21 +48,16 @@ class HomeViewController: UIViewController {
         infoView.hidden = true
         sosialView.hidden = false
         headerLabel.hidden = false
-        //segmentedControl.selectedSegmentIndex = 3
-
         self.callViewChangedToChildController(SosialViewController)
     }
     
     override func viewDidAppear(animated: Bool) {
         switch segmentedControl.selectedSegmentIndex{
-//        case 0:
-//            self.callViewChangedToChildController(NewsViewController)
         case 1:
             self.callViewChangedToChildController(InfoViewController)
         case 2:
             self.callViewChangedToChildController(SosialViewController)
         default: break
-//            print("erroe")
         }
     }
     
@@ -102,36 +94,24 @@ class HomeViewController: UIViewController {
     
     func handleSwipes(sender:UISwipeGestureRecognizer) {
         if (sender.direction == .Left) {
-            print("Swipe Left")
             if (viewShowing==0){
                 showInfoTab()
                 segmentedControl.selectedSegmentIndex = 1
             } else if (viewShowing==1){
                 showSosialTab()
                 segmentedControl.selectedSegmentIndex = 2
-            } else if (viewShowing==2){
-                print("allerede helt til venstre")
-            } else {
-                print("nothing")
             }
         }
         
         if (sender.direction == .Right) {
-            print("Swipe Right")
             if (viewShowing==0){
-                print("allerede helt til hoyre")
             } else if (viewShowing==1){
                 showNewsTab()
                 segmentedControl.selectedSegmentIndex = 0
-
             } else if (viewShowing==2){
                 showInfoTab()
                 segmentedControl.selectedSegmentIndex = 1
-
-            } else {
-                print("nothing")
             }
-            
         }
     }
 }
