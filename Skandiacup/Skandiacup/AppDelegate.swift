98672c142915366ed1b1aa309cfd4252225478cb
eiriksandberg <eirik.sandberg@live.no>
2015-10-14 14:27:28 +0200

    GUI bug fixes

--- Skandiacup/Skandiacup/AppDelegate.swift
+++ Skandiacup/Skandiacup/AppDelegate.swift
@@ -20,13 +20,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         navigationBarAppearace.tintColor = UIColor.darkGrayColor()
         
         
-        navigationBarAppearace.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
-        navigationBarAppearace.shadowImage = UIImage()
+        //navigationBarAppearace.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+       // navigationBarAppearace.shadowImage = UIImage()
         navigationBarAppearace.translucent = true
         
-        UITabBar.appearance().barTintColor = UIColor.clearColor()
-        UITabBar.appearance().backgroundImage = UIImage()
-        UITabBar.appearance().shadowImage = UIImage()
+       // UITabBar.appearance().barTintColor = UIColor(red: 41.0/255.0, green: 40.0/255.0, blue: 39.0/255.0, alpha: 1.0)
+        UITabBar.appearance().barTintColor = UIColor.whiteColor()
+        UITabBar.appearance().tintColor = UIColor(red:0.02, green:0.54, blue:0.02, alpha:1.0)
+        //UITabBar.appearance().backgroundImage = UIImage()
         
         return true
     }
