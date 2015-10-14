4a38f35bae464cc09a84d8433d00020c4bd56804
J√∏rgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-10-14 23:56:50 +0200

    Endplay not crashing when it contains games that doesnt have a result. Forgot to sync earlier

--- Skandiacup/Skandiacup/EndPlayGamesViewController.swift
+++ Skandiacup/Skandiacup/EndPlayGamesViewController.swift
@@ -66,9 +66,16 @@ class EndPlayGamesViewController: UITableViewController{
         let cellIdentifier = "endPlayMatchesInMatchClass"
         
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as UITableViewCell!
-        if endPlayMatchesInMatchClass[sortedKeys[indexPath.section]] != nil{
-            cell.textLabel?.text = "\(endPlayMatchesInMatchClass[sortedKeys[indexPath.section]]![indexPath.row].homeTeamName!) \(endPlayMatchesInMatchClass[sortedKeys[indexPath.section]]![indexPath.row].homegoal!)  - \(endPlayMatchesInMatchClass[sortedKeys[indexPath.section]]![indexPath.row].awaygoal!) \(endPlayMatchesInMatchClass[sortedKeys[indexPath.section]]![indexPath.row].awayTeamName!) "
+//        if endPlayMatchesInMatchClass[sortedKeys[indexPath.section]] != nil{
+        if let match = endPlayMatchesInMatchClass[sortedKeys[indexPath.section]]?[indexPath.row]{
+            if match.homegoal != nil{
+                cell.textLabel?.text = "\(match.homeTeamName!) \(match.homegoal!)  - \(match.awaygoal!) \(match.awayTeamName!) "
+            }else{
+                cell.textLabel?.text = "\(match.homeTeamName!) - \(match.awayTeamName!) "
+            }
+
         }
+        
         cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
         return cell
     }
