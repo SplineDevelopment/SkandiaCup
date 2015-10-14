a96aa1ff376f74330b4ef20e4634d3af0a766a25
Bj√∏rn Hoxmark <bhoxmark@gmail.com>
2015-10-14 16:32:41 +0200

    Swipe between home views.

--- Skandiacup/Skandiacup/HomeViewController.swift
+++ Skandiacup/Skandiacup/HomeViewController.swift
@@ -1,14 +1,7 @@
-//
-//  HomeViewController.swift
-//  Skandiacup
-//
-//  Created by Bjørn Hoxmark on 03/09/15.
-//  Copyright © 2015 Spline Development. All rights reserved.
-//
-
 import UIKit
 
 class HomeViewController: UIViewController {
+
 
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
@@ -16,32 +9,51 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var newsView: UIView!
     @IBOutlet weak var sosialView: UIView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    var viewShowing = 0;
 
     @IBAction func indexChanged(sender: UISegmentedControl) {
         switch segmentedControl.selectedSegmentIndex{
         case 0:
-            newsView.hidden = false
-            infoView.hidden = true
-            sosialView.hidden = true
-            headerLabel.hidden = true
-//            self.callViewChangedToChildController(NewsViewController)
+            showNewsTab()
+            //  self.callViewChangedToChildController(NewsViewController)
         case 1:
-            newsView.hidden = true
-            infoView.hidden = false
-            sosialView.hidden = true
-            headerLabel.hidden = true
-
-            self.callViewChangedToChildController(InfoViewController)
+            showInfoTab()
         case 2:
-            newsView.hidden = true
-            infoView.hidden = true
-            sosialView.hidden = false
-            headerLabel.hidden = false
-
-            self.callViewChangedToChildController(SosialViewController)
+            showSosialTab()
         default: break
 //            print("erroe")
         }
+    }
+    
+    func showNewsTab(){
+        viewShowing = 0;
+        newsView.hidden = false
+        infoView.hidden = true
+        sosialView.hidden = true
+        headerLabel.hidden = true
+        //segmentedControl.selectedSegmentIndex = 1
+    }
+    
+    func showInfoTab(){
+        viewShowing = 1;
+        newsView.hidden = true
+        infoView.hidden = false
+        sosialView.hidden = true
+        headerLabel.hidden = true
+        //segmentedControl.selectedSegmentIndex = 2
+       
+        self.callViewChangedToChildController(InfoViewController)
+    }
+    
+    func showSosialTab(){
+        viewShowing = 2;
+        newsView.hidden = true
+        infoView.hidden = true
+        sosialView.hidden = false
+        headerLabel.hidden = false
+        //segmentedControl.selectedSegmentIndex = 3
+
+        self.callViewChangedToChildController(SosialViewController)
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -75,6 +87,13 @@ class HomeViewController: UIViewController {
         activityIndicator.startAnimating()
         newsView.hidden = true
 
+        //Swipes
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        leftSwipe.direction = .Left
+        rightSwipe.direction = .Right
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
     }
 
     override func didReceiveMemoryWarning() {
@@ -82,15 +101,38 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func handleSwipes(sender:UISwipeGestureRecognizer) {
+        if (sender.direction == .Left) {
+            print("Swipe Left")
+            if (viewShowing==0){
+                showInfoTab()
+                segmentedControl.selectedSegmentIndex = 1
+            } else if (viewShowing==1){
+                showSosialTab()
+                segmentedControl.selectedSegmentIndex = 2
+            } else if (viewShowing==2){
+                print("allerede helt til venstre")
+            } else {
+                print("nothing")
+            }
+        }
+        
+        if (sender.direction == .Right) {
+            print("Swipe Right")
+            if (viewShowing==0){
+                print("allerede helt til hoyre")
+            } else if (viewShowing==1){
+                showNewsTab()
+                segmentedControl.selectedSegmentIndex = 0
 
-    /*
-    // MARK: - Navigation
+            } else if (viewShowing==2){
+                showInfoTab()
+                segmentedControl.selectedSegmentIndex = 1
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+            } else {
+                print("nothing")
+            }
+            
+        }
     }
-    */
-
 }
