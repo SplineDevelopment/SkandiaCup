aee6da9690713b73e3d07bc7bb6f2dfb4374a537
borgarlie <borgar.lie@gmail.com>
2015-10-22 11:34:55 +0200

    Added a timer to instagram

--- Skandiacup/Skandiacup/SosialViewController.swift
+++ Skandiacup/Skandiacup/SosialViewController.swift
@@ -14,6 +14,8 @@ class SosialViewController: UICollectionViewController, SegmentChangeProto {
     private let sectionInsets = UIEdgeInsets(top: 0.0, left: 11.0, bottom: 0.0, right: 0.0)
     private var insta_photos = [InstagramPhotoObject]()
     
+    var insta_timer = 0.0
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if (segue.identifier == "cellSegueInstaPopover") {
             let svc = segue.destinationViewController as! InstaPopover
@@ -25,15 +27,11 @@ class SosialViewController: UICollectionViewController, SegmentChangeProto {
     }
     
     func viewChangedTo() {
-        // remove -> replace with timer
-        if self.insta_photos.count > 0 {
+        // 2 min timer
+        if CACurrentMediaTime() < self.insta_timer + (60*2) {
 //            print("insta photos already initialized")
             return
         }
-        // TODO::: remove list and replace? or something else?
-        // need to be able to update based on a timer!
-        
-//        print("Loading data from instagram")
         let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
         dispatch_async(dispatch_get_global_queue(priority, 0)) {
             SharingManager.data.getAllPhotoObjects() { (photoObjects, error) -> () in
@@ -53,7 +51,8 @@ class SosialViewController: UICollectionViewController, SegmentChangeProto {
                         //vc.error_label.text = "Instagram not available atm"
                     }
                     else {
-                        self.insta_photos.appendContentsOf(photoObjects)
+                        self.insta_timer = CACurrentMediaTime()
+                        self.insta_photos = photoObjects
                         self.viewOutlet.reloadData()
                     }
                 }
