314114b9a7cb9742db9e431fed51db696c6dcac9
Bjørn Hoxmark <bhoxmark@gmail.com>
2015-12-19 11:49:59 +0100

    Added new button in instagramView: "open in instagram".

--- Skandiacup/Skandiacup/Config.swift
+++ Skandiacup/Skandiacup/Config.swift
@@ -15,6 +15,6 @@ class Config{
     static let filterViewHeight:CGFloat = 133
     static let teamCellHeight:CGFloat = 44
     static let matchCellViewHeight:CGFloat = 124
-    static let tag_name = "Norwaycup2014"
+    static let tag_name = "beachday"
     static let client_id_insta = "7b9d2e2f9ef04d81939c7c61f381184e"
 }
